f971b3c8624498d6774ebe8bf098768b8a962766
alexdum <alexandru.dumitrescu@gmail.com>
2022-11-24 17:43:59 +0200

    agric

--- ui.r
+++ ui.r
@@ -3,89 +3,14 @@ library(shiny)
 library(gridlayout)
 
 navbarPage(
-  title = "Chick Weights",
-  selected = "Agricultura",
+  title = "RO-Adapt explorer",
+  selected = "Despre",
   collapsible = TRUE,
-  theme = bslib::bs_theme(),
-  tabPanel(
-    title = "Agricultura",
-    tabsetPanel(
-      tabPanel(
-        title = "General",
-        grid_container(
-          layout = c(
-            "area0 area1",
-            ".     .    "
-          ),
-          row_sizes = c(
-            "0.79fr",
-            "1.21fr"
-          ),
-          col_sizes = c(
-            "0.47fr",
-            "1.53fr"
-          ),
-          gap_size = "10px",
-          grid_card(
-            area = "area0",
-            selectInput(
-              inputId = "mySelectInput",
-              label = "Select Input",
-              choices = list(
-                `choice a` = "a",
-                `choice b` = "b"
-              )
-            )
-          ),
-          grid_card(
-            area = "area1",
-            plotlyOutput(
-              outputId = "plot",
-              width = "100%",
-              height = "400px"
-            )
-          )
-        )
-      ),
-      tabPanel(
-        title = "Detalii",
-        grid_container(
-          layout = c(
-            "area0 area1",
-            ".     .    "
-          ),
-          row_sizes = c(
-            "1fr",
-            "1fr"
-          ),
-          col_sizes = c(
-            "0.47fr",
-            "1.53fr"
-          ),
-          gap_size = "10px",
-          grid_card(
-            area = "area0",
-            selectInput(
-              inputId = "mySelectInput",
-              label = "Select Input",
-              choices = list(
-                `choice a` = "a",
-                `choice b` = "b"
-              )
-            )
-          ),
-          grid_card(
-            area = "area1",
-            plotlyOutput(
-              outputId = "plot",
-              width = "100%",
-              height = "400px"
-            )
-          )
-        )
-      )
-    )
-  ),
+  theme = bslib::bs_theme(bootswatch = "minty"),
+  
+  source("sections/ui_agricultura.R",  local = TRUE)$value
+
+ ,
   tabPanel(
     title = "Energie",
     tabsetPanel(
