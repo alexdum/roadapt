c5160e569d372ad9a493ccfa17b392b53ae5d28e
alexdum <alexandru.dumitrescu@gmail.com>
2022-11-27 16:33:52 +0200

    maps

--- sections/server_agricultura.R
+++ sections/server_agricultura.R
@@ -1,5 +1,21 @@
 
 # harta leaflet -----------------------------------------------------------
+agr_rea <- reactive({
+  
+  indic <- input$agr_ind
+  scena <-  input$agr_scen
+  perio <- strsplit(input$agr_perio, "-")[[1]][2]
+  nc <- raster::brick(paste0("www/data/ncs/",indic,"Adjust_",scena,"_",perio,"-50_19710101_21001231.nc"))
+  nc
+  
+})
+
+
+output$test <- renderText({
+  paste("You chose", input$agr_ind)
+})
+
+
 output$agr_map <- renderLeaflet ({
   leaflet(
     #data = cities_map,
@@ -8,7 +24,7 @@ output$agr_map <- renderLeaflet ({
     ) 
   ) %>%
     leaflet.extras::addBootstrapDependency() %>%
-    setView(25, 46, zoom = 3) %>%
+    setView(25, 46, zoom = 6) %>%
     setMaxBounds(-12, 27.58, 56, 71.5) %>% 
     #addMapPane(name = "pol", zIndex = 410) %>%
     addMapPane(name = "maplabels", zIndex = 420) %>%
@@ -19,6 +35,10 @@ output$agr_map <- renderLeaflet ({
         onClick = JS("function(btn, map){ map.setView([46, 25], 3); }")
       )
     )   %>%
+    addRasterImage(
+      agr_rea()[[1]], opacity = .8
+      # options = leafletOptions(pane = "raster")
+    ) %>%
     addLayersControl(
       baseGroups = "CartoDB.PositronNoLabels",
       overlayGroups = c("Labels", "City borders"))  %>% 
