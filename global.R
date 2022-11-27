21b2b9f1354129855cdcf7a5b5013711a79a27bf
alexdum <alexandru.dumitrescu@gmail.com>
2022-11-27 16:34:26 +0200

    selection files

--- global.R
+++ global.R
@@ -2,3 +2,15 @@ library(shiny)
 library(gridlayout)
 library(ggplot2)
 library(leaflet)
+library(raster)
+
+
+select_agro_ind <- read.csv("www/data/tabs/agro/select_agro_ind.csv") 
+select_agro_ind  <- setNames(select_agro_ind$choice, select_agro_ind$parameter)
+
+select_interv <- read.csv("www/data/tabs/select_interv.csv") 
+select_interv <- setNames(select_interv$choice, select_interv$parameter)
+
+
+
+ 
