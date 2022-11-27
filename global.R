f389c2a6ad28a9b3ab379a548555e883734720f4
alexdum <alexandru.dumitrescu@gmail.com>
2022-11-27 19:12:48 +0200

    add fun read

--- global.R
+++ global.R
@@ -4,6 +4,7 @@ library(ggplot2)
 library(leaflet)
 library(raster)
 
+source("utils/names_to_date.R")
 
 select_agro_ind <- read.csv("www/data/tabs/agro/select_agro_ind.csv") 
 select_agro_ind  <- setNames(select_agro_ind$choice, select_agro_ind$parameter)
