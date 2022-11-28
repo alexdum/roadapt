190a458bd5b7c272615b12ac792741173e2c9a93
alexdum <alexandru.dumitrescu@gmail.com>
2022-11-28 19:15:22 +0200

    add uat function

--- global.R
+++ global.R
@@ -10,6 +10,7 @@ library(arrow)
 
 source("utils/names_to_date.R")
 source("utils/leaflet_fun.R")
+source("utils/leaflet_fun_det.R")
 
 
 borders <- st_read("www/data/shp/ne_10m_admin_0_boundary_lines_land_rou.shp")
@@ -17,6 +18,8 @@ mask <- vect("www/data/shp/rou_border_mbufer.shp")
 mask <- project(mask,  "EPSG:3857")
          
 
+uat <- st_read("www/data/shp/UAT_poli_2019_merc.shp", quiet = T) |> select(natCode) |>
+     st_transform(4326)
 
 select_agro_ind <- read.csv("www/data/tabs/agro/select_agro_ind.csv") 
 select_agro_ind  <- setNames(select_agro_ind$choice, select_agro_ind$parameter)
