58e0e5a9d6c467bdb6d3a9c7499671597fdf82e3
alexdum <alexandru.dumitrescu@gmail.com>
2022-11-29 16:56:14 +0200

    first commit

---
+++ utils/cols_leg_agr_det.R
@@ -0,0 +1,24 @@
+cols_leg_agr_det <- function(ind, perio_tip, domain) {
+  
+  # intervale si culori agricultura
+  if (ind == "pr") {
+    if (perio_tip == "year") {
+      bins <- seq(floor(min(domain)), ceiling(max(domain)), by = 100)
+    } else if (perio_sub == "season") {
+      bins <- seq(floor(min(domain)), ceiling(max(domain)), by = 50)
+    } else {
+      bins <- seq(floor(min(domain)), ceiling(max(domain)), by = 10)
+    }
+    pal <- colorBin("GnBu", domain = domain, bins = bins)
+    pal_rev <- colorBin("GnBu", domain = domain, bins = bins, reverse = T)
+    tit_leg <- "mm"
+    
+  } else {
+    bins <- seq(floor(min(domain)), ceiling(max(domain)), by = 2)
+    pal <- colorBin("RdYlBu", domain = domain, bins = bins, reverse = T)
+    pal_rev <- colorBin("RdYlBu", domain = domain, bins = bins,reverse = F)
+    tit_leg <- "°C"
+  }
+  
+  return(list(pal = pal, pal_rev = pal_rev, tit_leg = tit_leg))
+}
