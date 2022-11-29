291e2a7666dc03a5ec916cb095df122917897f30
alexdum <alexandru.dumitrescu@gmail.com>
2022-11-29 15:48:26 +0200

    add tabs_url

--- server.R
+++ server.R
@@ -1,6 +1,8 @@
 
 function(input, output, session) {
-
+  
+  source("utils/tabs_url.R", local = T)
   source("sections/server_agricultura_gen.R", local = T)
   source("sections/server_agricultura_det.R", local = T)
+  
 }
