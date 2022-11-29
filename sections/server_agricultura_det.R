37235f02ad87a63555335fc2c4041915784b38b3
alexdum <alexandru.dumitrescu@gmail.com>
2022-11-29 12:50:57 +0200

    add legend

--- sections/server_agricultura_det.R
+++ sections/server_agricultura_det.R
@@ -44,31 +44,77 @@ agr_rdet <- eventReactive(list(input$go_agrdet, isolate(input$tab_agro_det)),{
     }
   }
   
-  
+  # unire cu spatial
   uat.sub <- uat |> left_join(tab.sub, by = c( "natCode" = "ID"))
   
-  print(uat.sub)
   
   
-  
+  if (ind == "pr") {
+    
+    bins <- seq(floor(min(uat.sub$value)), ceiling(max(uat.sub$value)), by = 10)
+    pal <- colorBin("GnBu", domain = uat.sub$value, bins = bins)
+    pal_rev <- colorBin("GnBu", domain = uat.sub$value, bins = bins, reverse = T)
+    tit_leg <- "mm"
+    
+  } else {
+    bins <- seq(floor(min(uat.sub$value)), ceiling(max(uat.sub$value)), by = 2)
+    print(bins)
+    pal <- colorBin("RdYlBu", domain = uat.sub$value, bins = bins)
+    pal_rev <- colorBin("RdYlBu", domain = uat.sub$value, bins = bins,reverse = T)
+    tit_leg <- "°C"
+  }
   
   list(
-    uat.sub = uat.sub
+    uat.sub = uat.sub, pal = pal, pal_rev = pal_rev, tit_leg = tit_leg
   )
   
 })
-
-
 
 output$agr_map_det <- renderLeaflet ({
   
-  
-  qpal <- colorBin("Blues", domain = range(agr_rdet()$uat.sub$p50), bins = 4)
-  
-  #pal <- colorBin(cols, domain = agr_rdet()$uat.sub$value, bins = 4)
-  #pal2 <- colorBin(cols, domain = shape$values, bins = bins, reverse = T)
-  
   leaflet_fun_det(
-    data = agr_rdet()$uat.sub
+    data = isolate(agr_rdet()$uat.sub),
+    pal =  isolate(agr_rdet()$pal),
+    pal_rev =  isolate(agr_rdet()$pal_rev),
+    tit_leg = isolate(agr_rdet()$tit_leg)
   )
 })
+
+
+observe({
+  pal_rev =  agr_rdet()$pal_rev
+  tit_leg = agr_rdet()$tit_leg
+  req(input$go_agrdet) # Only display if tab is 'Detaliil'
+  data <- agr_rdet()$uat.sub
+  pal <- agr_rdet()$pal
+  
+  leafletProxy("agr_map_det",  data = data)  %>%
+    clearShapes() %>%
+    addPolygons (
+      fillColor = ~pal(value),
+      label = ~paste("<font size='2'><b>Region type: UAT<br/>Name units:",name_1,
+                     "<br/>",round(value,1),"</b></font><br/>
+                       <font size='1' color='#E95420'>Click to
+                       get values and graph</font>") %>% lapply(htmltools::HTML),
+      labelOptions = labelOptions(textsize = "13px"),
+      color = "grey",
+      weight = 0.5, smoothFactor = 0.1,
+      opacity = 0.5,
+      # fillOpacity = opacy ,
+      layerId = ~natCode,
+      # options = pathOptions(pane = "pol"),
+      #group = "region",
+      highlightOptions = highlightOptions(
+        weight = 2,
+        color = "#666",
+        fillOpacity = 0.2,
+        bringToFront = TRUE,
+        sendToBack = TRUE)) |>
+    clearControls() |>
+    addLegend(
+      title = paste0("<html>", gsub(",","",toString(rep("&nbsp;", 5))), tit_leg,"</html>"),
+      "bottomright", pal = pal_rev, values = ~values, opacity = 1,
+      labFormat = labelFormat(transform = function(x) sort(x, decreasing = TRUE))
+    ) 
+  
+}) 
