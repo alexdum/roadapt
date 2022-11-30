f7852dd3276376851d528c3a47280326257064d7
alexdum <alexandru.dumitrescu@gmail.com>
2022-11-30 17:24:14 +0200

    python function update

--- utils/extract_point.py
+++ utils/extract_point.py
@@ -3,6 +3,18 @@ import xarray as xr
 def extract_point(fname, lon, lat, variable):
   ds = xr.open_dataset(fname)
   ds.close()
-  dsloc = ds.sel(lon=lon,lat=lat,method='nearest')
-  data = dsloc[variable].to_pandas()
-  return(data)
+  dsloc = ds.sel(Longitude=lon,Latitude=lat,method='nearest')
+  dsloc = dsloc[variable].to_pandas() 
+  dsf = dsloc.rename_axis('index1').reset_index() # numele coloanei in coloana
+  dsf = dsf.rename({'index1':'time', 0:'value'}, axis = 'columns') # rename columns
+  return(dsf)
+
+
+
+
+
+# ds = xr.open_dataset("www/data/ncs/agro/prAdjust_rcp45_season-50_19710101_21001231.nc")
+# dsloc = ds.sel(Longitude=25,Latitude=46,method='nearest')
+# dsloc = dsloc["prAdjust"].to_pandas()
+# dsf = dsloc.rename_axis('index1').reset_index()
+# dsf.rename({'index1':'time', 0:'value'}, axis = 'columns')
