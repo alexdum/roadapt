487d1d7edcfab82b888aaa1ed32b59a7815aada4
alexdum <alexandru.dumitrescu@gmail.com>
2022-11-30 13:00:09 +0200

    improve show popup function

--- utils/show_popup.R
+++ utils/show_popup.R
@@ -1,9 +1,9 @@
 #show raster values on the map
-show_pop <- function(x = NULL, y = NULL, rdat = NULL, proxy = NULL) {
+show_popup <- function(x = NULL, y = NULL, rdat = NULL, proxy = NULL) {
   cell <- terra::cellFromXY(rdat, cbind(x, y))
   if (!is.na(cell)) {
     xy <- terra::xyFromCell(rdat, cell)
     val = rdat[cell]
-    if (!is.na(val)) proxy %>% clearPopups() %>% addPopups(xy[1],xy[2], popup = paste("LST: ", round(val,1)))
+    if (!is.na(val)) proxy %>% clearPopups() %>% addPopups(xy[1],xy[2], popup = paste0("lon:", round(x,3)," lat:", round(y,3), " val:", round(val,1)))
   }
 }
