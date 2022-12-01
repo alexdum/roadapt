4653947ed00e9e367aae6f0930f7e5c3558be4e2
alexdum <alexandru.dumitrescu@gmail.com>
2022-12-01 14:50:32 +0200

    tidy code

--- utils/extract_timeserie_gen.R
+++ utils/extract_timeserie_gen.R
@@ -14,19 +14,19 @@ extract_timeser_gen <- function(nc_fil,xy, perio_sub) {
   dd_min <-extract_point(fname = gsub("-50", "-min", nc_fil), lon = xy[1], lat = xy[2], variable = varia)
   
   dd <- dd_50 |> left_join(dd_max, by = c("time")) |> left_join(dd_min, by = c("time")) |>
-    dplyr::rename('med' = 'value.x', 'max' = 'value.y', 'min' = 'value')  |>
+    dplyr::rename('med' = 'value.x', 'max' = 'value.y', 'min' = 'value', 'data' = 'time')  |>
     # roling mean la extreme min max
     mutate(
       # max = zoo::rollmean(max, 3, na.pad = T) |> round(1),
       # min = zoo::rollmean(min, 3, na.pad = T) |> round(1), 
       med = round(med, 1), max = round(max, 1), min = round(min, 1),
-      data = as.Date(time)
+      data = as.Date(data)
     ) 
   
-  if (perio_sub != "year")  dd <- dd |> dplyr::filter(format(time, "%m") %in% perio_sub)
+  if (perio_sub != "year")  dd <- dd |> dplyr::filter(format(data, "%m") %in% perio_sub)
   
   dd <- dd |> mutate(
-    an = format(time, "%Y"),
+    an = format(data, "%Y"),
     change_med = case_when(
       varia %in% c("prAdjust", "ur") ~  (((med*100)/mean(med[an <= 2000])) - 100)  %>% round(1),
       !varia %in% c("prAdjust", "ur") ~   (med - mean(med[an <= 2000]))  %>% round(1)),
@@ -36,7 +36,7 @@ extract_timeser_gen <- function(nc_fil,xy, perio_sub) {
     change_min = case_when(
       varia %in% c("prAdjust", "ur") ~  (((min*100)/mean(med[an <= 2000])) - 100)  %>% round(1),
       !varia %in% c("prAdjust", "ur") ~   (min - mean(med[an <= 2000]))  %>% round(1)),
-    med_ref = mean(med)
+    med_ref = mean(med) |> round(1)
   )
   
   return(dd)
