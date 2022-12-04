d356541ce29076f3ee3af58aebe11cf21c0c8de6
alexdum <alexandru.dumitrescu@gmail.com>
2022-12-05 00:05:17 +0200

    add plot

--- sections/server_agricultura_det.R
+++ sections/server_agricultura_det.R
@@ -7,21 +7,21 @@ agr_rdet <- eventReactive(list(input$go_agrdet, isolate(input$tab_agro_det)),{
   perio_sub <- strsplit(input$agr_perio_det, "-")[[1]][1] # number of month
   perio_tip <- strsplit(input$agr_perio_det, "-")[[1]][2] # month/season/year
   tab <-  read_parquet(paste0("www/data/parquet/agro/", indic ,"Adjust_",scena,"_", perio_tip ,"-50_19710101_21001231.parquet"))
-
   
- an1_abat <- input$slider_agro_abate_det[1]
- an2_abat <- input$slider_agro_abate_det[2]
- an1_abs <- input$slider_agro_absol_det[1]
- an2_abs <- input$slider_agro_absol_det[2]
+  
+  an1_abat <- input$slider_agro_abate_det[1]
+  an2_abat <- input$slider_agro_abate_det[2]
+  an1_abs <- input$slider_agro_absol_det[1]
+  an2_abs <- input$slider_agro_absol_det[2]
   
   # calcul abateri absolute cu funct utils/calcul_agr_det.R
-  tab.sub <- calcul_agro_det(tab, agr_tip, perio_sub, indic, an1_abat, an2_abat, an1_abs, an2_abs)
+  tab_sub <- calcul_agro_det(tab, agr_tip, perio_sub, indic, an1_abat, an2_abat, an1_abs, an2_abs)
   # unire cu spatial
-  uat.sub <- uat |> left_join(tab.sub, by = c( "natCode" = "ID"))
-  #print(tab.sub)
- 
+  uat_sub <- uat |> left_join(tab_sub, by = c( "natCode" = "ID"))
+  print(head(tab_sub))
+  
   # legenda si intervale functie utils/cols_leg_agr_det.R
-  map_leg <- map_func_cols(indic, agr_tip, domain = range(uat.sub$value), perio_tip)
+  map_leg <- map_func_cols(indic, agr_tip, domain = range(uat_sub$value), perio_tip)
   
   
   # text harta
@@ -38,8 +38,9 @@ agr_rdet <- eventReactive(list(input$go_agrdet, isolate(input$tab_agro_det)),{
   )
   
   list(
-    uat.sub = uat.sub, pal = map_leg$pal, pal_rev = map_leg$pal_rev, tit_leg = map_leg$tit_leg,
-    param_text = param_text, opacy = input$transp_agr_det
+    uat_sub = uat_sub, pal = map_leg$pal, pal_rev = map_leg$pal_rev, tit_leg = map_leg$tit_leg,
+    param_text = param_text, opacy = input$transp_agr_det, tab = tab, perio_sub = perio_sub, indic = indic,
+    agr_tip = agr_tip
   )
   
 })
@@ -51,7 +52,7 @@ output$agr_text_det <- renderText({
 
 output$agr_map_det <- renderLeaflet ({
   leaflet_fun_det(
-    data = isolate(agr_rdet()$uat.sub),
+    data = isolate(agr_rdet()$uat_sub),
     pal =  isolate(agr_rdet()$pal),
     pal_rev =  isolate(agr_rdet()$pal_rev),
     tit_leg = isolate(agr_rdet()$tit_leg)
@@ -63,7 +64,7 @@ observe({
   #req(input$go_agrdet == "Detalii") # Only display if tab is 'Detalii'
   pal_rev =  agr_rdet()$pal_rev
   tit_leg = agr_rdet()$tit_leg
-  data <- agr_rdet()$uat.sub
+  data <- agr_rdet()$uat_sub
   pal <- agr_rdet()$pal
   opacy <- agr_rdet()$opacy 
   
@@ -97,3 +98,40 @@ observe({
     ) 
   
 }) 
+
+variables_plot_agro_det <- reactiveValues(
+  input = NULL, title = NULL, cors = NULL, indic = NULL, tip = NULL, 
+  # variabile control pentru actualizare grafic/date
+  update_input = NULL, update_input_tip_plt = NULL 
+)
+
+observeEvent(input$agr_map_det_shape_click$id,{ 
+  
+  id <- input$agr_map_det_shape_click$id
+  tab <- agr_rdet()$tab
+  perio_sub <- agr_rdet()$perio_sub
+  indic <- agr_rdet()$indic
+  tip <- agr_rdet()$agr_tip
+ 
+  dd <- extract_timeser_det(tab,id, perio_sub, indic)
+  print(head(dd))
+  variables_plot_agro_det$input <- dd
+  variables_plot_agro_det$indic <-  indic 
+  variables_plot_agro_det$tip <- tip
+  
+}) 
+
+
+# pentru subtab plot
+output$agro_timeseries_det_plot <- renderPlotly({
+  req(!is.na(variables_plot_agro_det$input))
+  print(head( variables_plot_agro_det$tip))
+  print(variables_plot_agro_det$tip)
+  print( variables_plot_agro_det$indic)
+  plt <- plots_agro_det(
+    variables_plot_agro_det$input,
+    variables_plot_agro_det$tip,  
+    variables_plot_agro_det$indic
+  )
+  plt$gp
+})
