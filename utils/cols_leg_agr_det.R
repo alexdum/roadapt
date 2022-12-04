0e5460a0add5fb2e8fd26b2b8da09bab39404a55
alexdum <alexandru.dumitrescu@gmail.com>
2022-12-04 17:36:58 +0200

    tidy code

--- utils/cols_leg_agr_det.R
+++ utils/cols_leg_agr_det.R
@@ -1,24 +1,23 @@
-cols_leg_agr_det <- function(indic, perio_tip, domain) {
+cols_leg_agr_det <- function(indic, perio_tip, perio_sub, domain) {
   
-  # intervale si culori agricultura
+  # intervale si culori agricultura detalii
   if (indic == "pr") {
     if (perio_tip == "year") {
-      bins <- seq(floor(min(domain)), ceiling(max(domain)), by = 100)
-    } else if (perio_sub == "season") {
-      bins <- seq(floor(min(domain)), ceiling(max(domain)), by = 50)
+      bins <- seq(floor(min(domain)), ceiling(max(domain)), by = 25)
+    } else if (perio_tip == "season") {
+      bins <- seq(floor(min(domain)), ceiling(max(domain)), by = 25)
     } else {
-      bins <- seq(floor(min(domain)), ceiling(max(domain)), by = 10)
+      bins <- seq(floor(min(domain)), ceiling(max(domain)), by = 5)
     }
-    pal <- colorBin("GnBu", domain = domain, bins = bins)
+    pal <- colorBin("GnBu", domain = domain, bins = bins, reverse = F)
     pal_rev <- colorBin("GnBu", domain = domain, bins = bins, reverse = T)
     tit_leg <- "mm"
     
   } else {
     bins <- seq(floor(min(domain)), ceiling(max(domain)), by = 2)
     pal <- colorBin("RdYlBu", domain = domain, bins = bins, reverse = T)
-    pal_rev <- colorBin("RdYlBu", domain = domain, bins = bins,reverse = F)
+    pal_rev <- colorBin("RdYlBu", domain = domain, bins = bins, reverse = F)
     tit_leg <- "°C"
   }
-  
   return(list(pal = pal, pal_rev = pal_rev, tit_leg = tit_leg))
 }
