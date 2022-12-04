f857397e998e0496957592b7e70c4c2c3664638a
alexdum <alexandru.dumitrescu@gmail.com>
2022-12-04 21:21:49 +0200

    use map funct

--- global.R
+++ global.R
@@ -17,10 +17,10 @@ library(DT)
 source_python("utils/extract_point.py")
 source("utils/utils.R")
 source("utils/names_to_date.R") # c]mp data din fisere netcdf
+source("utils/map_func_cols.R") # plot din date raster
 source("utils/leaflet_fun_gen.R") # harta tab general
 source("utils/leaflet_fun_det.R") # harta tab detalii
 source("utils/show_popup.R") # popup  date raster
-source("utils/cols_leg_agr_det.R") # culori legenda detalii
 source("utils/calcul_agr_gen.R") # calcul anomalii normale  detalii agro
 source("utils/calcul_agr_det.R") # calcul anomalii normale  detalii agro
 source("utils/extract_timeserie_gen.R") # extrage time serii pentru plot din ncs
