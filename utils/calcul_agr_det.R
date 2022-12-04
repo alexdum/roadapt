8a2b4254b4b1cb8f8db9efbf05d413361e79d98e
alexdum <alexandru.dumitrescu@gmail.com>
2022-12-04 17:36:13 +0200

    use correct anomalies computation

--- utils/calcul_agr_det.R
+++ utils/calcul_agr_det.R
@@ -1,23 +1,38 @@
 #subseteaza dup
-calcul_agro_det <- function(tab, agr_tip_det, perio_sub, an1_abat, an2_abat, an1_abs, an2_abs) {
+calcul_agro_det <- function(tab, agr_tip_det, perio_sub, indic, an1_abat, an2_abat, an1_abs, an2_abs) {
   #subseteaza dupa tip/an
   if (agr_tip_det == "abate") {
- 
+    
     
     if (perio_sub != "year") { #daca ai an formateaza data diferit
       tab.sub <- tab |> 
-        #filter( month ==  as.integer(perio_sub)) |>
+        filter( month ==  as.integer(perio_sub)) |>
+        group_by(ID) |>
         mutate(norm = mean(p50[year >= 1971 & year <= 2000])) |> 
         filter(year >= an1_abat & year <= an2_abat) |>
         group_by(ID) |> summarise(p50 = mean(p50) , norm = mean(norm)) |>
-        mutate(value = p50 - norm)
+        mutate(
+          value = case_when(
+            indic %in% c("pr", "ur") ~  (((p50*100)/norm) - 100)  %>% round(1),
+            !indic %in% c("pr", "ur") ~  (p50 - norm)  %>% round(1)
+          )
+        )
+      
     } else {
       tab.sub <- tab |> 
-        filter(year >= an1_abat & year <= an2_abat ) |>
-        group_by(ID) |> summarise(value = mean(p50)) 
+        group_by(ID) |>
+        mutate(norm = mean(p50[year >= 1971 & year <= 2000])) |> 
+        filter(year >= an1_abat & year <= an2_abat) |>
+        group_by(ID) |> 
+        summarise(p50 = mean(p50) , norm = mean(norm)) |>
+        mutate(
+          value = case_when(
+            indic %in% c("pr", "ur") ~  (((p50*100)/norm) - 100)  %>% round(1),
+            !indic %in% c("pr", "ur") ~ (p50 - norm)  %>% round(1)
+          )
+        )
     }
   } else {
- 
     
     if (perio_sub != "year") { #daca ai an formateaza data diferit
       tab.sub <- tab |> 
