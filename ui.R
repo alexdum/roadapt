b84eb2ba253838c709e4fdfde25bdd14c79658b6
alexdum <alexandru.dumitrescu@gmail.com>
2022-12-13 13:49:24 +0200

    ascunde titlu navbar

--- ui.R
+++ ui.R
@@ -6,16 +6,25 @@ source("sections/ui_agricultura.R",  local = TRUE)
 source("sections/ui_energie.R",  local = TRUE)
 source("sections/ui_despre.R",  local = TRUE)
 
+ui <- shinyUI(
+  tagList(  # ascunde titlu navbar
+    tags$head(tags$style(type = 'text/css','.navbar-brand{display:none;}')),
+    
+    navbarPage(
 
-navbarPage(
-  title = "RO-Adapt explorer",
-  id = "tabs",
-  selected = "#despre",
-  fluid = T,
-  collapsible = TRUE,
-  theme = bslib::bs_theme(bootswatch = "minty"),
-  
-  ui_agricultura,
-  ui_energie,
-  ui_despre
+      title = "RO-Adapt explorer",
+      id = "tabs",
+      selected = "#despre",
+      fluid = T,
+      #position = "fixed-top",
+      collapsible = TRUE,
+      theme = bslib::bs_theme(bootswatch = "minty"),
+      
+      
+      ui_agricultura,
+      ui_energie,
+      ui_despre
+      
+    )
+  )
 )
