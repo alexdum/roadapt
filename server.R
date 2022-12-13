b2b23aa84fa3f4252e1a1ef5ac143470410c0346
alexdum <alexandru.dumitrescu@gmail.com>
2022-12-13 15:55:14 +0200

    reconfigurare secțiuni

--- server.R
+++ server.R
@@ -2,7 +2,7 @@
 function(input, output, session) {
   
   source("utils/tabs_url.R", local = T)
-  source("sections/server_agricultura_gen.R", local = T)
-  source("sections/server_agricultura_det.R", local = T)
+  source("sections/server_climatgeneral_gen.R", local = T)
+  source("sections/server_climatgeneral_det.R", local = T)
   
 }
