4962234f7595b6abfe75100451132bde6493fabf
alexdum <alexandru.dumitrescu@gmail.com>
2022-12-13 21:28:48 +0200

    add agro indicators

--- global.R
+++ global.R
@@ -40,6 +40,8 @@ reg <- st_read("www/data/shp/regiuni.topojson", quiet = T) |> dplyr::rename(natc
 
 select_climgen_ind <- read.csv("www/data/tabs/climgen/select_climgen_ind.csv") 
 select_climgen_ind  <- setNames(select_climgen_ind$choice, select_climgen_ind$parameter)
+select_agro_ind <- read.csv("www/data/tabs/agro/select_agro_ind.csv") 
+select_agro_ind  <- setNames(select_agro_ind$choice, select_agro_ind$parameter)
 
 select_interv <- read.csv("www/data/tabs/select_interv.csv") 
 select_interv <- setNames(select_interv$choice, select_interv$parameter)
