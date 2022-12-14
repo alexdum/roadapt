8f0a45f0a8d27ff4a2ed35ff260d475735877829
alexdum <alexandru.dumitrescu@gmail.com>
2022-12-14 13:03:14 +0200

    add server agro

--- server.R
+++ server.R
@@ -4,5 +4,6 @@ function(input, output, session) {
   source("utils/tabs_url.R", local = T)
   source("sections/server_climatgeneral_gen.R", local = T)
   source("sections/server_climatgeneral_det.R", local = T)
+  source("sections/server_agro_gen.R", local = T)
   
 }
