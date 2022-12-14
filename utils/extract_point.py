c9486566fe266de77997f4931f4696d1fdb8020b
alexdum <alexandru.dumitrescu@gmail.com>
2022-12-14 13:04:04 +0200

    upate python convert days to integer

--- utils/extract_point.py
+++ utils/extract_point.py
@@ -1,4 +1,5 @@
 import xarray as xr
+import pandas as pd
 
 def extract_point(fname, lon, lat, variable):
   ds = xr.open_dataset(fname)
@@ -7,14 +8,20 @@ def extract_point(fname, lon, lat, variable):
   dsloc = dsloc[variable].to_pandas() 
   dsf = dsloc.rename_axis('index1').reset_index() # numele coloanei in coloana
   dsf = dsf.rename({'index1':'time', 0:'value'}, axis = 'columns') # rename columns
+  
+  if (variable =="cdd"): 
+     dsf["value"] = dsf["value"].dt.days
+     
   return(dsf)
 
 
 
 
 
-# ds = xr.open_dataset("www/data/ncs/agro/prAdjust_rcp45_season-50_19710101_21001231.nc")
+# ds = xr.open_dataset("www/data/ncs/agro/cdd_rcp45_year-50_19710101_21001231.nc")
 # dsloc = ds.sel(Longitude=25,Latitude=46,method='nearest')
-# dsloc = dsloc["prAdjust"].to_pandas()
+# dsloc = dsloc["cdd"].to_pandas()
 # dsf = dsloc.rename_axis('index1').reset_index()
-# dsf.rename({'index1':'time', 0:'value'}, axis = 'columns')
+# dsf = dsf.rename({'index1':'time', 0:'value'}, axis = 'columns')
+
+
