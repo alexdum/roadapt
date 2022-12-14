5f58d5b9bf0cc538e3bfe1f4c99705ff53352f52
alexdum <alexandru.dumitrescu@gmail.com>
2022-12-14 15:47:07 +0200

    update inputs

--- sections/ui_agricultura.R
+++ sections/ui_agricultura.R
@@ -1,10 +1,10 @@
 ui_agricultura <- tabPanel(
-  title = "Agricultura", icon = icon("tractor"),
+  title = "Agricultura",icon = icon("file"),
   value = "#agricultura", id = "#agricultura",
   tabsetPanel(
     tabPanel(
       id = "tab_agro_gen",
-      title = "Național",
+      title = h6("Național"),
       fluidRow(
         column(
           width = 2,
@@ -57,7 +57,6 @@ ui_agricultura <- tabPanel(
             min = 0, max = 1, ticks = F,
             value = 0.8, step = 0.1,
           ),
-          
           actionButton("go_agrogen", "Actualizare harta", icon("sync")),
           radioButtons( # radio button show values
             "radio_agro_gen", label = "Harta click funcționalitate",
@@ -71,7 +70,6 @@ ui_agricultura <- tabPanel(
         ),
         column(
           width = 7,
-          
           h6(textOutput("agro_text_gen"), style = "text-align:center;"),
           wellPanel(
             leafletOutput("agro_map_gen")
@@ -89,7 +87,7 @@ ui_agricultura <- tabPanel(
                 title = h6("Grafic"),
                 wellPanel(
                   plotlyOutput("agro_timeseries_gen_plot") |> withSpinner(size = 0.5),
-                  downloadLink('down_plot_regio_ind', label = 'Download  PNG')
+                  #downloadLink('down_plot_regio_ind', label = 'Download  PNG')
                 )
               ), 
               tabPanel(
@@ -100,7 +98,6 @@ ui_agricultura <- tabPanel(
                 )
               )
             ),
-            
           ),
           conditionalPanel(
             condition = "input.radio_agro_gen == 2 && output.condpan_agro_gen == 'nas'",
@@ -110,11 +107,102 @@ ui_agricultura <- tabPanel(
           )
         )
       )
-      
     ),
     tabPanel(
-      title = "Administrativ"
+      id = "tab_agro_det",
+      title = h6("Administrativ"),
+      fluidRow(
+        column(
+          width = 2,
+          selectInput(
+            inputId = "agro_admin_det",
+            label = "Unitate administrativă",
+            choices = c("Regiuni dezvoltare" = "reg",
+                        "Județe" = "jud",
+                        "UAT" = "uat"),
+            selected =  "uat"
+          ),
+          selectInput(
+            inputId = "agro_ind_det",
+            label = "Indicator",
+            choices = select_agro_ind,
+            selected =  select_agro_ind[1]
+          ),
+          selectInput(
+            inputId = "agro_tip_det",
+            label = "Tip",
+            choices = list(
+              `Valori absolute` = "absol",
+              `Schimbare` = "abate"
+            ),
+            selected = "abate"
+          ),
+          selectInput(
+            inputId = "agro_scen_det",
+            label = "Scenariu",
+            choices = list(
+              `RCP4.5` = "rcp45",
+              `RCP8.5` = "rcp85"
+            ), 
+            selected = " rcp45"
+          ),
+          selectInput(
+            inputId = "agro_perio_det",
+            label = "Luna/Sezon",
+            choices = select_interv,
+            selected =  select_interv[17]
+          ),
+          conditionalPanel(
+            condition = "input.agro_tip_det == 'abate'",
+            sliderInput(
+              "slider_agro_abate_det", label = "Interval calcul", min = 2006, 
+              max = 2100, value = c(2071, 2100), dragRange = T, ticks = F,
+              sep = "", step = 1)
+          ),
+          conditionalPanel(
+            condition = "input.agro_tip_det== 'absol'",
+            sliderInput(
+              "slider_agro_absol_det", label = "Interval calcul", min = 1971, 
+              max = 2100, value = c(1971, 2000), dragRange = T, ticks = F,
+              sep = "", step = 1)
+          ),
+          sliderInput(
+            "transp_agro_det", "Transparență poligon",
+            min = 0, max = 1, ticks = F,
+            value = 0.7, step = 0.1,
+          ),
+          actionButton("go_agrodet", "Actualizare harta", icon("sync"))
+        ),
+        column(
+          width = 7,
+          h6(textOutput("agro_text_det"), style = "text-align:center;"),
+          wellPanel(
+            leafletOutput("agro_map_det") |> withSpinner(size = 0.5)
+          ),
+          br(),
+          tabsetPanel(
+            fluidRow (
+              h6(textOutput("condpan_agro_det"), style = "text-align:center;"),
+              htmlOutput("agro_det_stat")
+            ),
+            tabPanel(
+              value = "Grafic",
+              title = h6("Grafic"),
+              wellPanel(
+                plotlyOutput("agro_timeseries_det_plot") |> withSpinner(size = 0.5),
+                #downloadLink('down_plot_regio_ind', label = 'Download  PNG')
+              )
+            ), 
+            tabPanel(
+              value = "Data",
+              title = h6("Data"),
+              wellPanel(
+                DT::dataTableOutput("agro_timeseries_det_data")
+              )
+            )
+          )
+        )
+      )
     )
   )
-  
 )
