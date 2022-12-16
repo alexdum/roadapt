8c3539bd7f6456c20e6339073a47b8fbaca08318
alexdum <alexandru.dumitrescu@gmail.com>
2022-12-16 12:37:46 +0200

    first commit

--- sections/ui_energie.R
+++ sections/ui_energie.R
@@ -1,9 +1,209 @@
-ui_energie <- tabPanel(
-  title = "Energie",icon = icon("solar-panel"),
-  value = "#energie", id = "#energie",
-  h5("Energia"),
+ui_agricultura <- tabPanel(
+  title = "Agricultura",icon = icon("solar-panel"),
+  value = "#Energie", id = "#energie",
+  h5("Energie"),
   tabsetPanel(
-    tabPanel(title = "Național"),
-    tabPanel(title = "Administrativ")
+    tabPanel(
+      id = "tab_energie_gen",
+      title = h6("Național"),
+      fluidRow(
+        column(
+          width = 2,
+          selectInput(
+            inputId = "energie_ind",
+            label = "Indicator",
+            choices = select_energie_ind,
+            selected =  select_energie_ind[1]
+          ),
+          selectInput(
+            inputId = "energie_tip",
+            label = "Tip",
+            choices = list(
+              `Valori absolute` = "absol",
+              `Schimbare` = "abate"
+            ),
+            selected = "abate"
+          ),
+          selectInput(
+            inputId = "energie_scen",
+            label = "Scenariu",
+            choices = list(
+              `RCP4.5` = "rcp45",
+              `RCP8.5` = "rcp85"
+            ), 
+            selected = " rcp45"
+          ),
+          selectInput(
+            inputId = "energie_perio",
+            label = "Luna/Sezon",
+            choices = select_interv[17],
+            selected =  select_interv[17]
+          ),
+          conditionalPanel(
+            condition = "input.energie_tip == 'abate'",
+            sliderInput(
+              "slider_energie_abate_gen", label = "Interval calcul", min = 2006, 
+              max = 2100, value = c(2071, 2100), dragRange = T, ticks = F,
+              sep = "", step = 1)
+          ),
+          conditionalPanel(
+            condition = "input.energie_tip == 'absol'",
+            sliderInput(
+              "slider_energie_absol_gen", label = "Interval calcul", min = 1971, 
+              max = 2100, value = c(1971, 2000), dragRange = T, ticks = F,
+              sep = "", step = 1)
+          ),
+          sliderInput(
+            "transp_energie_gen", "Transparență raster",
+            min = 0, max = 1, ticks = F,
+            value = 0.8, step = 0.1,
+          ),
+          actionButton("go_energiegen", "Actualizare harta", icon("sync")),
+          radioButtons( # radio button show values
+            "radio_energie_gen", label = "Harta click funcționalitate",
+            choices = 
+              list(
+                "Afișează valoare raster" = 1, 
+                "Grafic serie temporală (afișare sub hartă)" = 2
+              ), 
+            selected = 1
+          ),
+        ),
+        column(
+          width = 7,
+          h6(textOutput("energie_text_gen"), style = "text-align:center;"),
+          wellPanel(
+            leafletOutput("energie_map_gen")
+          ),
+          br(),
+          conditionalPanel( # show graphs only when data available
+            condition = "input.radio_energie_gen == 2 && output.condpan_energie_gen != 'nas'",
+            
+            tabsetPanel(
+              fluidRow (
+                h6(textOutput("condpan_energie_gen"), style = "text-align:center;")
+              ),
+              tabPanel(
+                value = "Grafic",
+                title = h6("Grafic"),
+                wellPanel(
+                  plotlyOutput("energie_timeseries_gen_plot") |> withSpinner(size = 0.5),
+                  #downloadLink('down_plot_regio_ind', label = 'Download  PNG')
+                )
+              ), 
+              tabPanel(
+                value = "Data",
+                title = h6("Data"),
+                wellPanel(
+                  DT::dataTableOutput("energie_timeseries_gen_data")
+                )
+              )
+            ),
+          ),
+          conditionalPanel(
+            condition = "input.radio_energie_gen == 2 && output.condpan_energie_gen == 'nas'",
+            wellPanel(
+              p("Trebuie selectată o regiune cu valori disponibile")
+            )
+          )
+        )
+      )
+    ),
+    tabPanel(
+      id = "tab_energie_det",
+      title = h6("Administrativ"),
+      fluidRow(
+        column(
+          width = 2,
+          selectInput(
+            inputId = "energie_admin_det",
+            label = "Unitate administrativă",
+            choices = c("Regiuni dezvoltare" = "reg",
+                        "Județe" = "jud",
+                        "UAT" = "uat"),
+            selected =  "uat"
+          ),
+          selectInput(
+            inputId = "energie_ind_det",
+            label = "Indicator",
+            choices = select_energie_ind,
+            selected =  select_energie_ind[1]
+          ),
+          selectInput(
+            inputId = "energie_tip_det",
+            label = "Tip",
+            choices = list(
+              `Valori absolute` = "absol",
+              `Schimbare` = "abate"
+            ),
+            selected = "abate"
+          ),
+          selectInput(
+            inputId = "energie_scen_det",
+            label = "Scenariu",
+            choices = list(
+              `RCP4.5` = "rcp45",
+              `RCP8.5` = "rcp85"
+            ), 
+            selected = " rcp45"
+          ),
+          selectInput(
+            inputId = "energie_perio_det",
+            label = "Luna/Sezon",
+            choices = select_interv[17], # avem doar anuale
+            selected =  select_interv[17]
+          ),
+          conditionalPanel(
+            condition = "input.energie_tip_det == 'abate'",
+            sliderInput(
+              "slider_energie_abate_det", label = "Interval calcul", min = 2006, 
+              max = 2100, value = c(2071, 2100), dragRange = T, ticks = F,
+              sep = "", step = 1)
+          ),
+          conditionalPanel(
+            condition = "input.energie_tip_det== 'absol'",
+            sliderInput(
+              "slider_energie_absol_det", label = "Interval calcul", min = 1971, 
+              max = 2100, value = c(1971, 2000), dragRange = T, ticks = F,
+              sep = "", step = 1)
+          ),
+          sliderInput(
+            "transp_energie_det", "Transparență poligon",
+            min = 0, max = 1, ticks = F,
+            value = 0.7, step = 0.1,
+          ),
+          actionButton("go_energiedet", "Actualizare harta", icon("sync"))
+        ),
+        column(
+          width = 7,
+          h6(textOutput("energie_text_det"), style = "text-align:center;"),
+          wellPanel(
+            leafletOutput("energie_map_det") |> withSpinner(size = 0.5)
+          ),
+          br(),
+          tabsetPanel(
+            fluidRow (
+              h6(textOutput("condpan_energie_det"), style = "text-align:center;"),
+              htmlOutput("energie_det_stat")
+            ),
+            tabPanel(
+              value = "Grafic",
+              title = h6("Grafic"),
+              wellPanel(
+                plotlyOutput("energie_timeseries_det_plot") |> withSpinner(size = 0.5),
+                #downloadLink('down_plot_regio_ind', label = 'Download  PNG')
+              )
+            ), 
+            tabPanel(
+              value = "Data",
+              title = h6("Data"),
+              wellPanel(
+                DT::dataTableOutput("energie_timeseries_det_data")
+              )
+            )
+          )
+        )
+      )
+    )
   )
 )
