2489f4bdf1937f6a2f2c67d8a24ced1cc025fd33
alexdum <alexandru.dumitrescu@gmail.com>
2022-12-16 12:40:33 +0200

    add energie

--- server.R
+++ server.R
@@ -6,5 +6,7 @@ function(input, output, session) {
   source("sections/server_climatgeneral_det.R", local = T)
   source("sections/server_agro_gen.R", local = T)
   source("sections/server_agro_det.R", local = T)
+  source("sections/server_energie_gen.R", local = T)
+  source("sections/server_energie_det.R", local = T)
   
 }
