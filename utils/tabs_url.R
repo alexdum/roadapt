e4d4770dfa56a094ec69aec154c4455d841af961
alexdum <alexandru.dumitrescu@gmail.com>
2023-01-18 15:31:55 +0200

    add hidro

--- utils/tabs_url.R
+++ utils/tabs_url.R
@@ -27,7 +27,7 @@ observeEvent(getUrlHash(), {
   # No work to be done if input$tabs and the hash are already the same
   if (hash == input$tabs) return()
   
-  valid <- c("#general","#agricultura","#energie","#despre")
+  valid <- c("#general","#agricultura","#energie","#hidro","#despre")
   
   if (hash %in% valid) {
     updateTabsetPanel(session, "tabs", hash)
