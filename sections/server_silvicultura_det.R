4aad32e9c8755690cf72d47be2d288236965a68c
alexdum <alexandru.dumitrescu@gmail.com>
2023-01-21 12:33:37 +0200

    add r20mm

--- sections/server_silvicultura_det.R
+++ sections/server_silvicultura_det.R
@@ -1,4 +1,22 @@
+# update select input year/season/month -----------------------------------
 
+observe({
+  indic <- input$silvicultura_ind_det
+  if (indic %in% c("wsgsmax")) {
+    # luni/sezona/an
+    updateSelectInput(
+      session, "silvicultura_perio_det",
+      choices = select_interv,
+      selected = select_interv[1]
+    )
+  } else { # doara anuala cand nu le ai pe celelalte
+    updateSelectInput(
+      session, "silvicultura_perio_det",
+      choices = select_interv[17],
+      selected = select_interv[17]
+    )
+  }
+})
 
 silvicultura_rdet <- eventReactive(list(input$go_silviculturadet, isolate(input$tab_silvicultura_det)),{
   
@@ -21,7 +39,7 @@ silvicultura_rdet <- eventReactive(list(input$go_silviculturadet, isolate(input$
   
   
   tab <-  read_parquet(paste0("www/data/parquet/",indic_path,"/",admin,"/", indic ,"_",scena,"_", perio_tip ,"-50_19710101_21001231.parquet"))
-
+  
   an1_abat <- input$slider_silvicultura_abate_det[1]
   an2_abat <- input$slider_silvicultura_abate_det[2]
   an1_abs <- input$slider_silvicultura_absol_det[1]
@@ -81,7 +99,7 @@ observe({
   
   leafletProxy("silvicultura_map_det",  data = data)  %>%
     clearShapes() %>%
-    addPolygons (
+    addPolygons(
       fillColor = ~pal(value),
       label = ~paste("<font size='2'><b>Region type: UAT<br/>Name units:",name,
                      "<br/>",round(value,1),"</b></font><br/>
@@ -126,9 +144,9 @@ observeEvent(list(isolate(input$go_silviculturadet), isolate(input$tab_silvicult
   variables_plot_silvicultura_det$admin  <- silvicultura_rdet()$admin
   admin_spat_sub <- silvicultura_rdet()$admin_spat_sub
   first_sel <- sample(1:nrow(admin_spat_sub), 1)
-  variables_plot_silvicultura_det$id <-admin_spat_sub$natcode[first_sel]
-  variables_plot_silvicultura_det$name <-admin_spat_sub$name[admin_spat_sub$natcode == variables_plot_silvicultura_det$id]
-  variables_plot_silvicultura_det$county <-admin_spat_sub$county[admin_spat_sub$natcode == variables_plot_silvicultura_det$id]
+  variables_plot_silvicultura_det$id <- admin_spat_sub$natcode[first_sel]
+  variables_plot_silvicultura_det$name <- admin_spat_sub$name[admin_spat_sub$natcode == variables_plot_silvicultura_det$id]
+  variables_plot_silvicultura_det$county <- admin_spat_sub$county[admin_spat_sub$natcode == variables_plot_silvicultura_det$id]
   
 })
 
@@ -137,15 +155,14 @@ observe({
   
   variables_plot_silvicultura_det$admin <- silvicultura_rdet()$admin
   
-  if(!isTRUE(all.equal(variables_plot_silvicultura_det$admin, variables_plot_silvicultura_det$update_admin)))  {
+  if (!isTRUE(all.equal(variables_plot_silvicultura_det$admin, variables_plot_silvicultura_det$update_admin)))  {
     admin <- variables_plot_silvicultura_det$admin
     admin_spat_sub <- silvicultura_rdet()$admin_spat_sub
     first_sel <- sample(1:nrow(admin_spat_sub), 1)
     variables_plot_silvicultura_det$id <- admin_spat_sub$natcode[first_sel]
-    variables_plot_silvicultura_det$name <-admin_spat_sub$name[admin_spat_sub$natcode == variables_plot_silvicultura_det$id]
-    variables_plot_silvicultura_det$county <-admin_spat_sub$county[admin_spat_sub$natcode == variables_plot_silvicultura_det$id]
-    variables_plot_silvicultura_det$update_admin <- admin
-    
+    variables_plot_silvicultura_det$name <- admin_spat_sub$name[admin_spat_sub$natcode == variables_plot_silvicultura_det$id]
+    variables_plot_silvicultura_det$county <- admin_spat_sub$county[admin_spat_sub$natcode == variables_plot_silvicultura_det$id]
+    variables_plot_silvicultura_det$update_admin <- adminx
   }
 })
 
@@ -154,8 +171,8 @@ observe({
 observeEvent(input$silvicultura_map_det_shape_click$id,{ 
   admin_spat_sub <- silvicultura_rdet()$admin_spat_sub
   variables_plot_silvicultura_det$id  <- input$silvicultura_map_det_shape_click$id
-  variables_plot_silvicultura_det$name <-admin_spat_sub$name[admin_spat_sub$natcode == input$silvicultura_map_det_shape_click$id]
-  variables_plot_silvicultura_det$county <-admin_spat_sub$county[admin_spat_sub$natcode == input$silvicultura_map_det_shape_click$id]
+  variables_plot_silvicultura_det$name <- admin_spat_sub$name[admin_spat_sub$natcode == input$silvicultura_map_det_shape_click$id]
+  variables_plot_silvicultura_det$county <- admin_spat_sub$county[admin_spat_sub$natcode == input$silvicultura_map_det_shape_click$id]
 })
 
 
@@ -206,11 +223,10 @@ output$silvicultura_det_stat <- renderUI({
   admin_spat_sub <- silvicultura_rdet()$admin_spat_sub
   silvicultura_tip <- silvicultura_rdet()$silvicultura_tip
   
-  if( silvicultura_tip == 'abate') {
-    norm <-admin_spat_sub$norm[admin_spat_sub$natcode == variables_plot_silvicultura_det$id] |> round(1)
-    multimean <-admin_spat_sub$p50[admin_spat_sub$natcode == variables_plot_silvicultura_det$id] |> round(1)
-    change <-admin_spat_sub$value[admin_spat_sub$natcode == variables_plot_silvicultura_det$id] |> round(1)
-    print(paste(an1_abat, an2_abat, norm, multimean, change))
+  if (silvicultura_tip == 'abate') {
+    norm <- admin_spat_sub$norm[admin_spat_sub$natcode == variables_plot_silvicultura_det$id] |> round(1)
+    multimean <- admin_spat_sub$p50[admin_spat_sub$natcode == variables_plot_silvicultura_det$id] |> round(1)
+    change <- admin_spat_sub$value[admin_spat_sub$natcode == variables_plot_silvicultura_det$id] |> round(1)
     
     HTML(
       paste0(
@@ -267,8 +283,8 @@ output$silvicultura_timeseries_det_plot <- renderPlotly({
 output$silvicultura_timeseries_det_data <- DT::renderDT({
   
   DT::datatable(
-    variables_plot_silvicultura_det$input 
-    |> dplyr:: mutate(across(is.numeric, round, digits = 1)) |>
+    variables_plot_silvicultura_det$input |> 
+      dplyr::mutate(across(is.numeric, round, digits = 1)) |>
       dplyr::select(-ID, -p10, -p90),
     extensions = 'Buttons', rownames = F,
     options = list(
