74778d141573e6832aa946dbf774b93872d7fa52
alexdum <alexandru.dumitrescu@gmail.com>
2023-01-30 15:03:39 +0200

    add turism

--- utils/tabs_url.R
+++ utils/tabs_url.R
@@ -23,16 +23,16 @@ observeEvent(input$tabs, {
 
 observeEvent(getUrlHash(), {
   hash <- getUrlHash()
-  
+
   # No work to be done if input$tabs and the hash are already the same
   if (hash == input$tabs) return()
-  
-  valid <- c("#general","#agricultura","#energie","#hidro", "#silvicultura","#biodivers", "#despre")
-  
+
+  valid <- c("#general","#agricultura","#energie","#hidro", "#silvicultura","#biodivers","#turism", "#despre")
+
   if (hash %in% valid) {
     updateTabsetPanel(session, "tabs", hash)
   }
-  
-  
-  
+
+
+
 })
