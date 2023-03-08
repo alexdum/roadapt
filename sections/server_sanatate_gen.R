1e3c9595955460b81d5ebe4d5fe989903f7f494d
alexdum <alexandru.dumitrescu@gmail.com>
2023-03-08 18:04:56 +0200

    update minmax

--- sections/server_sanatate_gen.R
+++ sections/server_sanatate_gen.R
@@ -35,18 +35,26 @@ sanatate_rea <- eventReactive(list(input$go_sanatategen, isolate(input$tab_sanat
   
   # citeste fisierul
   nc_fil <- paste0("www/data/ncs/",indic_path,"/",indic,"_",scena,"_",perio_tip,"-50_19710101_21001231.nc")
-
+  
+  
   # calcal abateri sau media multianuala cu functie calcul_sanatate_gen din utils
   ncf <- calcul_gen(nc_fil, sanatate_tip, perio_sub, indic, an1_abat, an2_abat, an1_abs, an2_abs)
-  
-  # pentru legenda titlu §i intervale §i culori
-  domain <- terra::minmax(ncf)
-  map_leg <- map_func_cols(indic, sanatate_tip, domain = domain, perio_tip)
-  
   
   # mask raster
   ncfm <- project(ncf,  "EPSG:3857", res = 5000, method = "near")
   ncfm <- terra::mask(ncfm, mask, touches = F)
+  
+  
+  # pentru legenda titlu si intervale si culori
+  min_max <- map_func_min_max(indic, sanatate_tip, perio_tip)
+  if (!is.na(min_max[1])) { # verifica daca ai valori disponibile, cel putin una
+    ncfm[ncfm > min_max[2]] <- min_max[2]
+    ncfm[ncfm < min_max[1]] <- min_max[1]
+  }
+  # pentru legenda titlu §i intervale §i culori
+  domain <- terra::minmax(ncfm)
+  map_leg <- map_func_cols(indic, sanatate_tip, domain = domain, perio_tip)
+  
   
   
   # text harta
